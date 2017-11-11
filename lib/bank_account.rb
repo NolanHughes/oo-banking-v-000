58c0d162cfd0198e76648a71cf090e0b380051e8
Nolan Hughes <nhughes987@gmail.com>
2017-11-11 21:48:58 +0000

    automatically backed up by learn

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -28,10 +28,10 @@ class BankAccount
 
   #valid if balance is less than 0 or status is "closed"
   def valid?
-    if @balance > 0 && @status = "open"
-      true
-    else
+    if @balance <= 0 || @status = "closed"
       false
+    else
+      true
     end
   end
 end
